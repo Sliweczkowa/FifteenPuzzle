071fb220b2684bb521e8a94dfb99e3bb6089ed84
heroesluk <heroesluk@gmail.com>
2024-10-27 18:45:47 +0100

    feat: refactor

--- src/frame.py
+++ src/frame.py
@@ -4,44 +4,32 @@ from typing import List
 class Frame:
 
     def __init__(self, r: int, c: int, vals: List[int]):
-        self.row = r
-        self.column = c
-        self.values = vals
+        self.row_count = self.set_row_count(r)
+        self.column_count = self.set_column_count(r)
+        self.game_board = self.validate_game_board(vals)
+        self.winning_board = [i for i in range(r * c)]
 
-    @property
-    def row(self):
-        return self._row
-
-    @row.setter
-    def row(self, row):
+    def set_column_count(self, row):
         if type(row) is not int:
             raise Exception("row must be of type int")
-        # TODO: add exception for minimal value
-        self._row = row
+        self.row_count = row
 
-    @property
-    def column(self):
-        return self._column
-
-    @column.setter
-    def column(self, col):
+    def set_row_count(self, col):
         if type(col) is not int:
             raise Exception("column must be of type int")
         # TODO: add exception for minimal value
-        self._column = col
+        self.column_count = col
 
-    @property
-    def values(self):
-        return self._values
-
-    @values.setter
-    def values(self, values):
-        if len(values) is not self.row*self.column:
+    def validate_game_board(self, new_board):
+        if len(new_board) is not self.row_count * self.column_count:
             raise Exception("values len must be the product of row and column")
-        if not all(type(v) is int for v in values):
+        if not all(type(v) is int for v in new_board):
             raise Exception("all values items must be of type int")
-        if not all( 0 < v < self.row*self.column for v in values):
-            raise Exception("all values items must be of value <0, row*column)")
-        if len(values) is not len(set(values)):
+        if not all(-1 < v < self.row_count * self.column_count for v in new_board):
+            raise Exception("all values items must be of value <-1, row*column)")
+        if len(new_board) is not len(set(new_board)):
             raise Exception("values items must not repeat")
-        self._values = values
+        self.game_board = new_board
+
+    def validate_win(self) -> bool:
+        return self.winning_board == self.game_board
