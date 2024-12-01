9ddc3b3fd4cd961c525a24afdc3f2d9f4d5c89b6
sliweczkowa <w.sliwinska@outlook.com>
2024-12-01 17:14:47 +0100

    feat: additional heuristics

--- src/algorithms/a_star.py
+++ src/algorithms/a_star.py
@@ -35,8 +35,22 @@ def a_star(frame: Frame, id_of_heuristic: int, weight: float) -> list[chr]:
                 if tile_number != 0:
                     tile_pos = divmod(index, column_no)  # current position
                     tile_win_pos = divmod(tile_number - 1, column_no)  # p.ex. tile "1" at index 0
-                    manhattan_distance = abs(tile_win_pos[0] - tile_pos[0]) + abs(tile_win_pos[1] - tile_pos[1])
-                    h += manhattan_distance
+                    if id_of_heuristic == 0:
+                        x = abs(tile_win_pos[0] - tile_pos[0])
+                        y = abs(tile_win_pos[1] - tile_pos[1])
+                        manhattan_distance = x + y
+                        h += manhattan_distance
+                    elif id_of_heuristic == 1:
+                        x = abs(tile_win_pos[0] - tile_pos[0])
+                        y = abs(tile_win_pos[1] - tile_pos[1])
+                        if x > y:
+                            diagonal_shortcut = 14 * y + 10 * (x - y)
+                        else:
+                            diagonal_shortcut = 14 * x + 10 * (y - x)
+                        h += diagonal_shortcut
+                    elif id_of_heuristic == 2:
+                        if tile_pos[0] != tile_win_pos[0] or tile_pos[1] != tile_win_pos[1]:
+                            h += 1
 
             # Calculate estimate of total cost
             frame = Frame(r=row_no, c=column_no, vals=board, cost=starting_frame[0].cost + 1)
